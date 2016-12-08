c45c4b6434eaae1f7794bf3d322a73675a5d98df
Mohammed Abdelaziz <maa643@nyu.edu>
2016-12-08 13:31:31 -0500

    About to phase out manual dt setting, major changes

--- hydroCode/tests.py
+++ hydroCode/tests.py
@@ -1,16 +1,16 @@
 import numpy as np
+import pylab as plt
 
-A = np.zeros([7,7])
-A[0:3,:] = 1.
-A[3::,:] = 0.5
-A[2:5,2:5] = np.random.rand(3,3)
+xPoints = np.linspace(0,2*np.pi,100)
+yPoints = np.linspace(0,2*np.pi,100)
 
-print 'array'
-print A
-print 'avg btwn rows'
-print (A[1:,:] + A[:-1,:])/2.
-print 'avg btwn cols'
-print (A[:,1:]+A[:,:-1])/2.
+xVary = np.sin(xPoints)
+yVary = np.cos(yPoints)
+X,Y = np.meshgrid(xPoints,yPoints)
 
-print 'summed over overlapping region'
-print (A[1:,:] + A[:-1,:])/2. + (A[:,1:]+A[:,:-1])/2.
+test = xVary[:,None]*yVary[None,:]
+test2 = xVary*(xVary[:,None]**2 + yVary[None,:]**2)
+
+
+plt.contourf(X,Y,test2)
+plt.show()
--- hydroCode/twoDimHighOrder.py
+++ hydroCode/twoDimHighOrder.py
@@ -33,55 +33,60 @@ def FHLLFunc(U,F,n):
 	# Calculate HLL Flux as an array FHLLH[:,:,0:2] and FHLLV at all interfaces (3 less entries than cell centers for 2nd order space)
 	
 	rho = U[:,:,n,0]
-	v = U[:,:,n,1]/U[:,:,n,0]
+	vx = U[:,:,n,1]/U[:,:,n,0]
+	vy = U[:,:,n,2]/U[:,:,n,0]
 	p = F[:,:,n,1] - U[:,:,n,1]**2 / U[:,:,n,0]
-	lambdaPlusL , lambdaMinusL ,lambdaPlusR ,lambdaMinusR ,lambdaPlusD, lambdaMinusD, lambdaPlusU, lambdaMinusU = lambdaFunc(v,gamma,p,rho)
+	lambdaPlusL , lambdaMinusL ,lambdaPlusR ,lambdaMinusR ,lambdaPlusD, lambdaMinusD, lambdaPlusU, lambdaMinusU = lambdaFunc(vx,vy,gamma,p,rho)
 	alphaPlusH ,alphaMinusH, alphaPlusV, alphaMinusV = alphaFunc(lambdaPlusL,lambdaMinusL,lambdaPlusR,lambdaMinusR,lambdaPlusD,lambdaMinusD,lambdaPlusU,lambdaMinusU)
-	print alphaPlusH.shape, alphaPlusV.shape
-	lefts, rights, downs, ups = interpolate(v,gamma,p,rho)
-	FHLLH = np.zeros([Nx-3,Ny,Nt,3])
 
-	UL = np.zeros([Nx-3,Ny,3])
-	UR = np.zeros([Nx-3,Ny,3])
-	FL = np.zeros([Nx-3,Ny,3])
-	FR = np.zeros([Nx-3,Ny,3])
-	EL = lefts[:,:,0]/(gamma-1) + 0.5* lefts[:,:,1]*lefts[:,:,2]**2  # energy (used multiple times)
-	ER = rights[:,:,0]/(gamma-1) + 0.5* rights[:,:,1]*rights[:,:,2]**2 
+	lefts, rights, downs, ups = interpolate(vx,vy,gamma,p,rho)
+	FHLLH = np.zeros([Nx-3,Ny,Nt,4])
+
+	UL = np.zeros([Nx-3,Ny,4])
+	UR = np.zeros([Nx-3,Ny,4])
+	FL = np.zeros([Nx-3,Ny,4])
+	FR = np.zeros([Nx-3,Ny,4])
+	EL = lefts[:,:,0]/(gamma-1) + 0.5* lefts[:,:,1]*(lefts[:,:,2]**2+lefts[:,:,3]**2)  # energy (used multiple times)
+	ER = rights[:,:,0]/(gamma-1) + 0.5* rights[:,:,1]*(rights[:,:,2]**2 +rights[:,:,3]**2)
 	UL[:,:,0], UR[:,:,0] = lefts[:,:,1], rights[:,:,1]
 	UL[:,:,1], UR[:,:,1] = lefts[:,:,1]*lefts[:,:,2] , rights[:,:,1]*rights[:,:,2]
-	UL[:,:,2], UR[:,:,2] = EL , ER
+	UL[:,:,2], UR[:,:,2] = lefts[:,:,1]*lefts[:,:,3] , rights[:,:,1]*rights[:,:,3]
+	UL[:,:,3], UR[:,:,3] = EL , ER
 	FL[:,:,0], FR[:,:,0] = lefts[:,:,1]*lefts[:,:,2] , rights[:,:,1]*rights[:,:,2]
 	FL[:,:,1], FR[:,:,1] = lefts[:,:,1]*lefts[:,:,2]**2 + lefts[:,:,0] , rights[:,:,1]*rights[:,:,2]**2 + rights[:,:,0] 
-	FL[:,:,2], FR[:,:,2] = (EL + lefts[:,:,0])*lefts[:,:,2] , (ER + rights[:,:,0])*rights[:,:,2]
+	FL[:,:,2], FR[:,:,2] =  lefts[:,:,1]*lefts[:,:,2]*lefts[:,:,3] , rights[:,:,1]*rights[:,:,2]*rights[:,:,3]
+	FL[:,:,3], FR[:,:,3] = (EL + lefts[:,:,0])*lefts[:,:,2] , (ER + rights[:,:,0])*rights[:,:,2]
 
 	FHLLH[:,:,n,:] = (alphaPlusH[:,:,None]*FL[:,:,:] + alphaMinusH[:,:,None]*FR[:,:,:] - alphaPlusH[:,:,None]*alphaMinusH[:,:,None] *(UR[:,:,:]- UL[:,:,:]))/(alphaPlusH[:,:,None] + alphaMinusH[:,:,None])
 
-	FHLLV = np.zeros([Nx,Ny-3,Nt,3])
-	UD = np.zeros([Nx,Ny-3,3])
-	UU = np.zeros([Nx,Ny-3,3])
-	FD = np.zeros([Nx,Ny-3,3])
-	FU = np.zeros([Nx,Ny-3,3])
-	ED = downs[:,:,0]/(gamma-1) + 0.5* downs[:,:,1]*downs[:,:,2]**2  # energy (used multiple times)
-	EU = downs[:,:,0]/(gamma-1) + 0.5* ups[:,:,1]*ups[:,:,2]**2 
+	FHLLV = np.zeros([Nx,Ny-3,Nt,4])
+	UD = np.zeros([Nx,Ny-3,4])
+	UU = np.zeros([Nx,Ny-3,4])
+	GD = np.zeros([Nx,Ny-3,4])
+	GU = np.zeros([Nx,Ny-3,4])
+	ED = downs[:,:,0]/(gamma-1) + 0.5* downs[:,:,1]*(downs[:,:,2]**2+downs[:,:,3]**2) # energy (used multiple times)
+	EU = downs[:,:,0]/(gamma-1) + 0.5* ups[:,:,1]*(ups[:,:,2]**2 +ups[:,:,3]**2)
 	UD[:,:,0], UU[:,:,0] = downs[:,:,1], ups[:,:,1]
 	UD[:,:,1], UU[:,:,1] = downs[:,:,1]*downs[:,:,2] , ups[:,:,1]*ups[:,:,2]
-	UD[:,:,2], UU[:,:,2] = ED , EU
-	FD[:,:,0], FU[:,:,0] = downs[:,:,1]*downs[:,:,2] , ups[:,:,1]*ups[:,:,2]
-	FD[:,:,1], FU[:,:,1] = downs[:,:,1]*downs[:,:,2]**2 + downs[:,:,0] , ups[:,:,1]*ups[:,:,2]**2 + ups[:,:,0] 
-	FD[:,:,2], FU[:,:,2] = (ED + downs[:,:,0])*downs[:,:,2] , (EU + ups[:,:,0])*ups[:,:,2]
+	UD[:,:,2], UU[:,:,2] = downs[:,:,1]*downs[:,:,3] , ups[:,:,1]*ups[:,:,3]
+	UD[:,:,3], UU[:,:,3] = ED , EU
+	GD[:,:,0], GU[:,:,0] = downs[:,:,1]*downs[:,:,3] , ups[:,:,1]*ups[:,:,3]
+	GD[:,:,1], GU[:,:,1] = downs[:,:,1]*downs[:,:,2]*downs[:,:,3], ups[:,:,1]*ups[:,:,2]*ups[:,:,3]
+	GD[:,:,2], GU[:,:,2] = downs[:,:,1]*downs[:,:,3]**2 + downs[:,:,0] , ups[:,:,1]*ups[:,:,3]**2 + ups[:,:,0]
+	GD[:,:,3], GU[:,:,3] = (ED + downs[:,:,0])*downs[:,:,3] , (EU + ups[:,:,0])*ups[:,:,3]
 	
-	FHLLV[:,:,n,:] = (alphaPlusV[:,:,None]*FD[:,:,:] + alphaMinusV[:,:,None]*FU[:,:,:] - alphaPlusV[:,:,None]*alphaMinusV[:,:,None] *(UU[:,:,:]- UD[:,:,:]))/(alphaPlusV[:,:,None] + alphaMinusV[:,:,None])
+	FHLLV[:,:,n,:] = (alphaPlusV[:,:,None]*GD[:,:,:] + alphaMinusV[:,:,None]*GU[:,:,:] - alphaPlusV[:,:,None]*alphaMinusV[:,:,None] *(UU[:,:,:]- UD[:,:,:]))/(alphaPlusV[:,:,None] + alphaMinusV[:,:,None])
 	
 	return FHLLH, FHLLV
 	
-def lambdaFunc(v,gamma,p,rho):
+def lambdaFunc(vx,vy,gamma,p,rho):
 	# Calculate lambda as an array at all positions lambdas[:,:] at some time 
 	# v[:,:] at all positions
 	# gamma constant
 	# p[:,:] at all positions	
 	# rho[:,:] at all positions
 
-	lefts, rights, downs, ups  = interpolate(v,gamma,p,rho)
+	lefts, rights, downs, ups  = interpolate(vx,vy,gamma,p,rho)
 	
 	soundL = np.sqrt(gamma*lefts[:,:,0]/lefts[:,:,1])
 	soundR = np.sqrt(gamma*rights[:,:,0]/rights[:,:,1])
@@ -111,37 +116,43 @@ def alphaFunc(lambdaPlusL,lambdaMinusL,lambdaPlusR,lambdaMinusR,lambdaPlusD,lamb
 	#print alphaPlusV
 	return alphaPlusH ,alphaMinusH, alphaPlusV, alphaMinusV
 
-def energyFunc(p,gamma,rho,v):
+def energyFunc(p,gamma,rho,vx,vy):
 	# Calculate energy in terms of p, gamma, rho, v
-	return p/(gamma-1) + 0.5* rho*v**2
+	return p/(gamma-1) + 0.5* rho*(vx*vx+vy*vy)
 
 def fluxUpdate(U,F,n):
-	# Calculate updated values of F based on a new U
+	# Calculate updated values of F and G based on a new U
 	newRho  = U[:,:,n+1,0]
-	newRhoV = U[:,:,n+1,1]
-	newV    = newRhoV/newRho
-	newE    = U[:,:,n+1,2]
-	newP    = (gamma-1) * (newE - 0.5*newRho*newV**2)
+	newRhoVx = U[:,:,n+1,1]
+	newRhoVy = U[:,:,n+1,2]
+	newVx    = newRhoVx/newRho
+	newVy   = newRhoVy/newRho
+	newE    = U[:,:,n+1,3]
+	newP    = (gamma-1) * (newE - 0.5*newRho*(newVx**2+newVy**2))
 	
-	F[:,:,n+1,0] = newRho*newV
-	F[:,:,n+1,1] = newRho*newV**2 + newP
-	F[:,:,n+1,2] = (newE+newP)*newV
+	F[:,:,n+1,0] = newRho*newVx
+	F[:,:,n+1,1] = newRho*newVx**2 + newP
+	F[:,:,n+1,2] = newRho*newVx*newVy
+	F[:,:,n+1,3] = (newE+newP)*newVx
+	
 	return F
 	
 def minmod(x,y,z):
 	# Minmod function
 	return 0.25*np.fabs(np.sign(x) + np.sign(y))*(np.sign(x) + np.sign(z)) * np.minimum.reduce([np.fabs(x),np.fabs(y),np.fabs(z)])
 	
-def interpolate(v,gamma,p,rho):
+def interpolate(vx,vy,gamma,p,rho):
 	#Interpolate states at interface sides for second order
 	theta = 1.5
-	cs = np.zeros([Nx,Ny,3]) # Cell States (values at cell centers)
+	cs = np.zeros([Nx,Ny,4]) # Cell States (values at cell centers)
 	cs[:,:,0] = p
 	cs[:,:,1] = rho
-	cs[:,:,2] = v
+	cs[:,:,2] = vx 
+	cs[:,:,3] = vy
 	
 	lefts  = cs[1:-2,:,:] + 0.5* minmod(theta*(cs[1:-2,:,:] - cs[0:-3,:,:]) , 0.5*(cs[2:-1,:,:] - cs[0:-3,:,:]) , theta*(cs[2:-1,:,:] - cs[1:-2,:,:]))
 	rights = cs[2:-1,:,:] - 0.5* minmod(theta*(cs[2:-1,:,:] - cs[1:-2,:,:]) , 0.5*(cs[3::,:,:] - cs[1:-2,:,:]) , theta*(cs[3::,:,:] - cs[2:-1,:,:]))
+	
 	
 	downs  = cs[:,1:-2,:] + 0.5* minmod(theta*(cs[:,1:-2,:] - cs[:,0:-3,:]) , 0.5*(cs[:,2:-1,:] - cs[:,0:-3,:]) , theta*(cs[:,2:-1,:] - cs[:,1:-2,:]))
 	ups    = cs[:,2:-1,:] - 0.5* minmod(theta*(cs[:,2:-1,:] - cs[:,1:-2,:]) , 0.5*(cs[:,3::,:] - cs[:,1:-2,:]) , theta*(cs[:,3::,:] - cs[:,2:-1,:]))
@@ -151,18 +162,20 @@ def interpolate(v,gamma,p,rho):
 ## COMPUTATION PARAMETERS
 tMin    = 0.
 tMax    = 1.0
-Nt      = 200
+Nt      = 500
 dt      = (tMax-tMin)/Nt
 tPoints = np.linspace(tMin,tMax,Nt)
 
-xMin    = -0.5
-xMax    =  0.5
-Nx      = 30
+#CFL = 1.0
+
+xMin    = 0.
+xMax    =  1.
+Nx      = 64
 dx      = (xMax - xMin)/Nx
 
 yMin    = 0.
 yMax    = 1.
-Ny      = 30
+Ny      = 64
 dy      = (yMax - yMin)/Ny
 
 
@@ -177,77 +190,29 @@ v0    = 0.0
 
 rhoL = 1.0
 pL   = 1.0
-vL   = 0.0
+vxL   = 0.0
+vyL =0.0
 pR   = 0.125
 rhoR = 0.1
-vR   = 0.0
+vxR   = 0.0
+vyR = 0.0
+U = np.zeros([Nx,Ny,Nt,4])
+F = np.zeros([Nx,Ny,Nt,4])
 
-U = np.zeros([Nx,Ny,Nt,3])
-F = np.zeros([Nx,Ny,Nt,3])
-FHLL = np.zeros([Nx-3,Ny-3,Nt,3]) #N-3 interfaces for N cells with 2 ghost cells on each end
+FHLL = np.zeros([Nx-3,Ny-3,Nt,4]) #N-3 interfaces for N cells with 2 ghost cells on each end
 U1 = np.zeros(U.shape)
 U2 = np.zeros(U.shape)
 
-### Left state
-#U[0:Nx/2,:,0,0] = rhoL
-#U[0:Nx/2,:,0,1] = rhoL*vL
-#U[0:Nx/2,:,0,2] = energyFunc(pL,gamma,rhoL,vL)
-
-#F[0:Nx/2,:,0,0] = rhoL*vL
-#F[0:Nx/2,:,0,1] = rhoL*vL**2 + pL
-#F[0:Nx/2,:,0,2] = (energyFunc(pL,gamma,rhoL,vL) + pL)*vL
-
-### Right State
-#U[Nx/2::,:,0,0] = rhoR
-#U[Nx/2::,:,0,1] = rhoR*vR
-#U[Nx/2::,:,0,2] = energyFunc(pR,gamma,rhoR,vR)
-
-#F[Nx/2::,:,0,0] = rhoR*vR
-#F[Nx/2::,:,0,1] = rhoR*vR**2 + pR
-#F[Nx/2::,:,0,2] = (energyFunc(pR,gamma,rhoR,vR) + pR)*vR
-
-
-### Left state
-#U[:,0:Nx/2,0,0] = rhoL
-#U[:,0:Nx/2,0,1] = rhoL*vL
-#U[:,0:Nx/2,0,2] = energyFunc(pL,gamma,rhoL,vL)
-
-#F[:,0:Nx/2,0,0] = rhoL*vL
-#F[:,0:Nx/2,0,1] = rhoL*vL**2 + pL
-#F[:,0:Nx/2,0,2] = (energyFunc(pL,gamma,rhoL,vL) + pL)*vL
-
-### Right State
-#U[:,Nx/2:,0,0] = rhoR
-#U[:,Nx/2:,0,1] = rhoR*vR
-#U[:,Nx/2:,0,2] = energyFunc(pR,gamma,rhoR,vR)
-
-#F[:,Nx/2:,0,0] = rhoR*vR
-#F[:,Nx/2:,0,1] = rhoR*vR**2 + pR
-#F[:,Nx/2:,0,2] = (energyFunc(pR,gamma,rhoR,vR) + pR)*vR
-
-## Up
-#for i in range(3,Nx-3):
-#	U[i,i,0,0] = rho0/2.
-#	U[Nx-i,i,0,0] = rho0/2.
-#	U[i,i,0,1] = rho0*v0/2.
-#	U[Nx-i,i,0,1] = rho0*v0/2.
-#	U[i,i,0,2] = energyFunc(p0,gamma,rho0/2.,v0)
-#	U[Nx-i,i,0,2] = energyFunc(p0,gamma,rho0/2.,v0)
-#	
-#	F[i,i,0,0] = rho0*v0/2.
-#	F[Nx-i,i,0,0] = rho0*v0/2.
-#	F[i,i,0,1] = rho0/2.*v0**2 + p0
-#	F[Nx-i,i,0,1] = rho0/2.*v0**2 + p0
-#	F[i,i,0,2] = (energyFunc(p0,gamma,rho0/2.,v0) + p0)*v0
-#	F[Nx-i,i,0,2] = (energyFunc(p0,gamma,rho0/2.,v0) + p0)*v0
-
-#U, F = initialConds.sod1D(U,F,gamma, rhoL,pL,vL, rhoR,pR,vR)
-U, F = initialConds.bessel(U,F,xPoints,gamma ,1.0,1.0,1.0)
+#U, F = initialConds.sod1D(U,F,gamma, rhoL,pL,vxL,vyL, rhoR,pR,vxR,vyR)
+#U, F = initialConds.bessel(U,F,xPoints,gamma ,1.0,1.0,1.0)
+#U,F =   initialConds.kelvHelm(U,F,xPoints,yPoints,gamma,2.5,1.0,2.0,-0.5,0.5,0.035,10**(-1))
+U,F = initialConds.kelvHelm2(U,F,xPoints,yPoints,gamma,1.0,1.0,10**(-2),10**(-1),4)
 
 U1 = U.copy() # Need to maintain BCS for U1 and U2 as well
 U2 = U.copy()
 F1 = F.copy()
 F2 = F.copy()
+
 
 ## MAIN LOOP
 count = 0 # Track loop number
@@ -263,49 +228,74 @@ for n in range(Nt-1):
 	F1 = fluxUpdate(U1,F,n-1)
 
 	U2[2:-2,2:-2,n,:]  = (3./4.)*U[2:-2,2:-2,n,:] + (1./4.)*U1[2:-2,2:-2,n,:] + (1./4.)*dt*LFunc(U1,F1,dx,dy,n)
-	F2 = fluxUpdate(U2,F1,n-1)
+	F2= fluxUpdate(U2,F1,n-1)
 
 	U[2:-2,2:-2,n+1,:] = (1./3.)*U[2:-2,2:-2,n,:] + (2./3.)*U2[2:-2,2:-2,n,:] + (2./3.)*dt*LFunc(U2,F2,dx,dy,n)
 	# Update all fluxes based on this update
 	F = fluxUpdate(U,F,n)
 	#print U[:,:,n,0]
-#	# Reinforce BCS (return ghost cells to initial conditions) (Need two ghost cells on each side for 2nd order spatial)
-	U[0:2,:,n+1,:]  = U[0:2,:,0,:]
-	F[0:2,:,n+1,:]  = F[0:2,:,0,:]
-	U[-2::,:,n+1,:] = U[-2::,:,0,:]
-	F[-2::,:,n+1,:] = F[-2::,:,0,:]
-	
-	U[:,0:2,n+1,:]  = U[:,0:2,0,:]
-	F[:,0:2,n+1,:]  = F[:,0:2,0,:]
-	U[:,-2::,n+1,:] = U[:,-2::,0,:]
-	F[:,-2::,n+1,:] = F[:,-2::,0,:]
+##	# Reinforce BCS (return ghost cells to initial conditions) (Need two ghost cells on each side for 2nd order spatial)
+#	U[0:2,:,n+1,:]  = U[0:2,:,0,:]
+#	F[0:2,:,n+1,:]  = F[0:2,:,0,:]
 
-	# Try Outflow BCS
-	#U[0,:,n+1,:]  = U[2,:,n+1,:]
-	#F[0,:,n+1,:]  = F[2,:,n+1,:]
-	#U[-1,:,n+1,:] = U[-3,:,n+1,:]
-	#F[-1,:,n+1,:] = F[-3,:,n+1,:]
+#	U[-2::,:,n+1,:] = U[-2::,:,0,:]
+#	F[-2::,:,n+1,:] = F[-2::,:,0,:]
+
+#	
+#	U[:,0:2,n+1,:]  = U[:,0:2,0,:]
+#	F[:,0:2,n+1,:]  = F[:,0:2,0,:]
+
+#	U[:,-2::,n+1,:] = U[:,-2::,0,:]
+#	F[:,-2::,n+1,:] = F[:,-2::,0,:]
+
+#	# Try Outflow BCS
+#	U[0,:,n+1,:]  = U[2,:,n+1,:]
+#	F[0,:,n+1,:]  = F[2,:,n+1,:]
+#	U[-1,:,n+1,:] = U[-3,:,n+1,:]
+#	F[-1,:,n+1,:] = F[-3,:,n+1,:]
+#	
+#	U[:,0,n+1,:]  = U[:,2,n+1,:]
+#	F[:,0,n+1,:]  = F[:,2,n+1,:]
+#	U[:,-1,n+1,:] = U[:,-3,n+1,:]
+#	F[:,-1,n+1,:] = F[:,-3,n+1,:]
+#	
+#	U[1,:,n+1,:]  = U[2,:,n+1,:]
+#	F[1,:,n+1,:]  = F[2,:,n+1,:]
+#	U[-2,:,n+1,:] = U[-3,:,n+1,:]
+#	F[-2,:,n+1,:] = F[-3,:,n+1,:]
+#	
+#	U[:,1,n+1,:]  = U[:,2,n+1,:]
+#	F[:,1,n+1,:]  = F[:,2,n+1,:]
+#	U[:,-2,n+1,:] = U[:,-3,n+1,:]
+#	F[:,-2,n+1,:] = F[:,-3,n+1,:]
+
+	# Try fixed BCS in x, periodic in y
+	U[0,:,n+1,:]  = U[0,:,0,:]
+	F[0,:,n+1,:]  = F[0,:,0,:]
+	U[-1,:,n+1,:] = U[-1,:,0,:]
+	F[-1,:,n+1,:] = F[-1,:,0,:]
 	
-	#U[:,0,n+1,:]  = U[:,2,n+1,:]
-	#F[:,0,n+1,:]  = F[:,2,n+1,:]
-	#U[:,-1,n+1,:] = U[:,-3,n+1,:]
-	#F[:,-1,n+1,:] = F[:,-3,n+1,:]
+	U[:,0,n+1,:]  = (U[:,-3,n+1,:]+U[:,2,n+1,:])/2.
+	F[:,0,n+1,:]  = (F[:,-3,n+1,:]+F[:,2,n+1,:])/2.
+	U[:,-1,n+1,:] = (U[:,-3,n+1,:]+U[:,2,n+1,:])/2.
+	F[:,-1,n+1,:] = (F[:,-3,n+1,:]+F[:,2,n+1,:])/2.
 	
-	#U[1,:,n+1,:]  = U[2,:,n+1,:]
-	#F[1,:,n+1,:]  = F[2,:,n+1,:]
-	#U[-2,:,n+1,:] = U[-3,:,n+1,:]
-	#F[-2,:,n+1,:] = F[-3,:,n+1,:]
+	U[1,:,n+1,:]  = U[1,:,0,:]
+	F[1,:,n+1,:]  = F[1,:,0,:]
+	U[-2,:,n+1,:] = U[-2,:,0,:]
+	F[-2,:,n+1,:] = F[-2,:,0,:]
 	
-	#U[:,1,n+1,:]  = U[:,2,n+1,:]
-	#F[:,1,n+1,:]  = F[:,2,n+1,:]
-	#U[:,-2,n+1,:] = U[:,-3,n+1,:]
-	#F[:,-2,n+1,:] = F[:,-3,n+1,:]
-	F = fluxUpdate(U,F,n)
+	U[:,1,n+1,:]  =(U[:,-3,n+1,:]+U[:,2,n+1,:])/2.
+	F[:,1,n+1,:]  = (F[:,-3,n+1,:]+F[:,2,n+1,:])/2.
+	U[:,-2,n+1,:] = (U[:,-3,n+1,:]+U[:,2,n+1,:])/2.
+	F[:,-2,n+1,:] =(F[:,-3,n+1,:]+F[:,2,n+1,:])/2.
+	F= fluxUpdate(U,F,n)
 	U1 = U.copy()
 	U2 = U.copy()
 	F1 = F.copy()
 	F2 = F.copy()
-	if count % 100 == 0:
+
+	if count % 50 == 0:
 		print count*100/Nt , '% Done'
 		
 #print F[:,:,0,2]
@@ -313,12 +303,13 @@ for n in range(Nt-1):
 
 ## PLOTTING
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.gca()
 
 X, Y = np.meshgrid(xPoints, yPoints)
 
 
-surf = ax.plot_surface(X[:,:],Y[:,:],U[:,:,-1,2], rstride=1, cstride=1,cmap=cm.coolwarm) #plots 
+surf = plt.contourf(X[:,:],Y[:,:],U[:,:,-1,0]) #plots 
+plt.colorbar(surf)
 
 #print 'Final Density'
 #print U[:,:,-1,0]
@@ -330,12 +321,14 @@ plt.ylim([yMin ,yMax])
 plt.xlim([xMin ,xMax])
 #ax.set_zlim([rhoR, rhoL])
 #plt.legend(loc = 'center')
-ax.view_init(elev=10., azim=60)
+#ax.view_init(elev=10., azim=60)
 
 
 def animate(i):
 	ax.clear() # Seems necessary to prevent data overlapping between frames
-	ax.plot_surface(X[:,:],Y[:,:],U[:,:,i,2], rstride=1, cstride=1,cmap=cm.coolwarm)   # update the data
+	plt.imshow(U[:,:,i,0] , origin = 'lower')   # update the data
+	plt.xlabel('X Position' , fontsize = 18)
+	plt.ylabel('Y Position', fontsize =18)
 	#ax.set_zlim([rhoR, rhoL])
 	return surf,
 
@@ -345,10 +338,10 @@ def init():
 	ax.plot_surface([],[],[], rstride=5, cstride=5,cmap=cm.coolwarm)  
 	return surf,
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1,Nt,2),  interval=25, blit=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(1,Nt,3),  interval=25, blit=False)
 
 
-#ani.save('my_animation.mp4')                           
+ani.save('my_animation.mp4')                           
 plt.show()
 
 
