de33e37c3a91111af9a032943342d4d72bd223a9
Mohammed Abdelaziz <maa643@nyu.edu>
2016-12-08 20:01:32 -0500

    Kind of working, instability issues, try simpler ICs

--- hydroCode/tests.py
+++ hydroCode/tests.py
@@ -1,16 +1,11 @@
 import numpy as np
-import pylab as plt
-
-xPoints = np.linspace(0,2*np.pi,100)
-yPoints = np.linspace(0,2*np.pi,100)
-
-xVary = np.sin(xPoints)
-yVary = np.cos(yPoints)
-X,Y = np.meshgrid(xPoints,yPoints)
-
-test = xVary[:,None]*yVary[None,:]
-test2 = xVary*(xVary[:,None]**2 + yVary[None,:]**2)
 
 
-plt.contourf(X,Y,test2)
-plt.show()
+rhos = np.random.rand(4,3)
+rhos2 = np.random.rand(3,4)
+
+rhoMax = max(rhos.max(),rhos2.max())
+
+print rhos
+print rhos2
+print rhoMax
